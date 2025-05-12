60825b74de2ed72d5791869144328d4bbe5d0482
teflondervish <slava.semenov03@mail.ru>
2025-05-12 21:58:35 +0500

    Initial commit

---
+++ src/main/java/ru/semenov/germesplusfabric/GermesPlusFabricApplication.java
@@ -0,0 +1,13 @@
+package ru.semenov.germesplusfabric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermesPlusFabricApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GermesPlusFabricApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/semenov/germesplusfabric/GermesPlusFabricApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.semenov.germesplusfabric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermesPlusFabricApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
